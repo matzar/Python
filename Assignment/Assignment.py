b88af9cb49835882677e8138dee605e01204a5ad
matzar <mat.zar@icloud.com>
2019-11-15 18:16:28 +0000

    add week 10 stuff

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -205,13 +205,15 @@ chapter_list = [word.split('Chapter') for word in book_words]
 
 # %%
 
-word = 'Chapter aaaa.... Chapter bbb bbbb.,b---b bbb bbb bbb b Chapter cc ccccccc ccc c c c c c c c ccc c a c- . sc '
+testt = book_words[:10]
 
-# test = word.split('Chapter')
-# test = [i for j in word for i in re.split(r'Chapter',j)]
-sentences = [i for j in word for i in j.split(' ')]
+new_bloke_in_town = dict()
 
-display(test)
+for word in testt:
+    for i in word:
+        if ()
+
+display(testt)
 # %% [markdown]
 # ## 9) Remove the first entry from the chapter list [6]
 # The first item in the chapter list is just the preface and chapter list. Copy that into a separate string variable and then remove it from the chapter list
