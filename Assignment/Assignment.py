63d39b98f60b3e5c5b8f909479734da8e8675653
matzar <mat.zar@icloud.com>
2019-11-16 17:36:10 +0000

    15 and 13 done

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -404,6 +404,36 @@ m
 # 
 
 # %%
+An attempt to improve the cities mentioned in the book was made. To my best knowledge a succesful one but 
+the analysis could be further improved by plotting only the cities Phileas Fogg visited.
+A full journey with connections plotted between the cities could be made as well. 
+The connections could show the amount of time the journey took; This could be done interactively with a mouse hover
+over the link showing the journey's time. We could also show the time it would take today to travel that link and
+maybe even show up-to-date tain and airplane schedules, as if Phileas was traveling right now and today; 
+Google Maps would be a great help in this. We can get the travel calcualtion, time and locations.
+Google on its official github account has google maps python intergration tools (https://github.com/googlemaps/google-maps-services-python)
+and it features an example usage:
+#%%
+import googlemaps
+from datetime import datetime
+
+gmaps = googlemaps.Client(key='Add Your Key here')
+
+# Geocoding an address
+geocode_result = gmaps.geocode('1600 Amphitheatre Parkway, Mountain View, CA')
+
+# Look up an address with reverse geocoding
+reverse_geocode_result = gmaps.reverse_geocode((40.714224, -73.961452))
+
+# Request directions via public transit
+now = datetime.now()
+directions_result = gmaps.directions("Sydney Town Hall",
+                                     "Parramatta, NSW",
+                                     mode="transit",
+                                     departure_time=now)
+#%%
+
+
 
 
 
