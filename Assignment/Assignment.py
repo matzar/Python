fb0801418fc91e1b7636ef4317167f9642e388ca
matzar <mat.zar@icloud.com>
2019-11-17 03:01:42 +0000

    bar plot working

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -278,24 +278,16 @@ import matplotlib.pyplot as plt; plt.rcdefaults()
 import numpy as np
 import matplotlib.pyplot as plt
 
-#%%
-objects = list()
-performace = list()
-for i in range(len(chapters_and_content)):
-    objects.append(chapters_and_content[i][0])
-    performace.append(len(chapters_and_content[i][1].split()))
+# create a list of chapters' lengths
+chapter_length = [len(chapters_and_content[i][1].split()) for i in range(len(chapters_and_content))]
 
-# y_pos = np.arange(len(objects))
-# performance = [10,8,6,4,2,1]
+y_pos = np.arange(len(chapter_list))
+plt.bar(y_pos, chapter_length, align='center', alpha=0.5)
+plt.xticks(y_pos, chapter_list)
+plt.ylabel('Usage')
+plt.title('The length of each chapter')
 
-# plt.bar(y_pos, performance, align='center', alpha=0.5)
-# plt.xticks(y_pos, objects)
-# plt.ylabel('Usage')
-# plt.title('Programming language usage')
-
-# plt.show()
-
-
+plt.show()
 
 # %%
 # print chapters
