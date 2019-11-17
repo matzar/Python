faf886ac95fbbbe4c8157a6977529667010dcef5
matzar <mat.zar@icloud.com>
2019-11-17 02:50:24 +0000

    chapters in a list

--- Assignment/Assignment.py
+++ Assignment/Assignment.py
@@ -274,11 +274,30 @@ for i in range(len(table_of_contents)):
 # - Use whatever plotting library you like. Matplotlib is fine.
 
 #%%
-# import re
-# import numpy as np
-# from matplotlib import pyplot as plt
-# %matplotlib inline
-print(len(chapters_and_content[0][1]))
+import matplotlib.pyplot as plt; plt.rcdefaults()
+import numpy as np
+import matplotlib.pyplot as plt
+
+#%%
+# print(len(chapters_and_content[0][1].split()))
+
+
+# objects = ('Python', 'C++', 'Java', 'Perl', 'Scala', 'Lisp')
+objects = list()
+for i in range(len(table_of_contents)):
+    objects.append(table_of_contents[i][0])
+
+display(objects)
+# y_pos = np.arange(len(objects))
+# performance = [10,8,6,4,2,1]
+
+# plt.bar(y_pos, performance, align='center', alpha=0.5)
+# plt.xticks(y_pos, objects)
+# plt.ylabel('Usage')
+# plt.title('Programming language usage')
+
+# plt.show()
+
 
 
 # %%
